7fe4f1e528563dc14fc7f355aaad4bcfd795ca12
saumya140 <105532306+saumya140@users.noreply.github.com>
2023-06-23 17:16:06 +0800

    Update status_prediction_web_app.py

--- status_prediction_web_app.py
+++ status_prediction_web_app.py
@@ -41,55 +41,24 @@ with st.sidebar:
 if (selected == 'Status Prediction'):
     
     # page title
-    st.title('Campus Placement by using SVM Model')
-    
-    
-
+    st.title('Campus Placement by using Model')
     gender = st.text_input('Gender (Male: 1, Female: 0)', key='gender')
-    
-    
     ssc_p = st.text_input('Secondary Education percentage - 10th Grade', key='ssc_p')
-    
-  
     ssc_b = st.text_input('Board of Education(10th Grade) : Central: 1, Others: 0 ', key='ssc_b')
-    
-   
     hsc_p = st.text_input('Higher Secondary Education percentage- 12th Grade', key='hsc_p')
- 
-    
     hsc_b = st.text_input('Board of Education(12th Grade) :Central: 1, Others: 0 ', key='hsc_b')
-
-    
     hsc_s = st.text_input('Specialization in Higher Secondary Education : Commerse: 2, Science: 1, Arts: 0 (hsc_s)', key='hsc_s')
-
-
     degree_p = st.text_input('Degree Percentage ', key='degree_p')
-    
-    
     degree_t = st.text_input('Under Graduation(Degree type)- Field of degree education : Sci&tech: 1, Comm&Mgmt: 2, Others: 3 ', key='degree_t')
-    
-   
     workex = st.text_input('Work Experiance : Yes: 1, No: 0 ', key='workex')
-    
-    
     etest_p = st.text_input('Employability test percentage (conducted by college)', key='etest_p')
-    
-    
     specialisation = st.text_input('Post Graduation(MBA) - Specialization : Mkt&Fin: 1, Mkt&HR: 0 ', key='specialisation')
-        
-
     mba_p = st.text_input('MBA Percentage', key='mba_p')
-
     # code for Prediction
     placement = ''
-    
     # creating a button for Prediction
-    
     if st.button('Placement Status Result'):
-        placement = status_prediction([gender, ssc_p , ssc_b , hsc_p , hsc_b , hsc_s , degree_p , degree_t , workex , etest_p ,specialisation ,	mba_p])
-        
-        
-        
+        placement = status_prediction([gender, ssc_p , ssc_b , hsc_p , hsc_b , hsc_s , degree_p , degree_t , workex , etest_p ,specialisation ,	mba_p])    
     st.success(placement)
 
 # Graph 1 Page
